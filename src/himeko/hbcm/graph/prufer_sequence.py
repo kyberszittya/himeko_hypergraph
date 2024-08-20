bb2cef750ecba7647c5189669abedd5dcbec3bc1
Hajdu Csaba <kyberszittya@protonmail.ch>
2024-08-20 23:04:38 +0200

    Finally, Prüfer sequence reconstruction works

--- src/himeko/hbcm/graph/prufer_sequence.py
+++ src/himeko/hbcm/graph/prufer_sequence.py
@@ -1,3 +1,4 @@
+import typing
 from collections import deque
 
 
@@ -8,11 +9,11 @@ def micikievus_code(root):
     fringe = deque()
     degree_map = {}
     for leaf in leafs:
-        fringe.append(leaf)
+        fringe.appendleft(leaf)
         degree_map[leaf] = 1
     # Deo & Micikevicius sequence
     while len(fringe) != 0:
-        node = fringe.popleft()
+        node = fringe.pop()
         u = node.parent
         if u is not None:
             # Node (deleted leaf), code
@@ -21,10 +22,10 @@ def micikievus_code(root):
                 degree_map[u] = u.count_composite_elements + 1
             degree_map[u] -= 1
             if degree_map[u] == 1:
-                fringe.append(u)
+                fringe.appendleft(u)
 
 
-def create_permutation(code, root):
+def create_permutation_map(code, root):
     """
     Create permutation from Prüfer-like code for proper indexing
     :param code:
@@ -37,3 +38,39 @@ def create_permutation(code, root):
         perm[node] = i
         perm_reverse[i] = node
     return perm, perm_reverse
+
+
+def create_permutation_sequence(code, permutation):
+    """
+    Create permutation from Prüfer-like code for proper indexing
+    :param code:
+    :return:
+    """
+    # Create permutation
+    for i, (node, _) in enumerate(code):
+        yield permutation[node]
+
+
+
+def reconstruct_naive_prufer(code, node_guids, node_dict: typing.Dict):
+    """
+    Reconstruct tree from Prüfer-like code
+    :param code: Prüfer sequence
+    :param node_guids: Nodes (GUIDS)
+    :return:
+    """
+    # Create permutation
+    n = len(code) + 2
+    print(node_guids)
+    degree = {}
+    for x in node_guids:
+        degree[x] = 1
+    for x in code:
+        degree[x] += 1
+    for i in range(len(code)):
+        for j in range(n):
+            if degree[j] == 1:
+                degree[j] -= 1
+                degree[code[i]] -= 1
+                #node_dict[j].add_child(node_dict[code[i]])
+                break
