2d8ca999519bb5d6213468d412d32d39df3774de
Hajdu Csaba <kyberszittya@protonmail.ch>
2024-08-25 20:55:46 +0200

    JSX template generated and works perfectly

--- src/himeko/transformations/mxw/mxw_scene.py
+++ src/himeko/transformations/mxw/mxw_scene.py
@@ -1,4 +1,7 @@
 import os
+import math
+import typing
+from dataclasses import dataclass
 
 from jinja2 import Environment, FileSystemLoader
 
@@ -8,13 +11,95 @@ from himeko.hbcm.factories.creation_elements import FactoryHypergraphElements
 from himeko.hbcm.queries.composition import QueryIsStereotypeOperation
 
 
+@dataclass
+class MxwNode(object):
+    name: str
+    position: typing.Optional[list]
+    orientation: typing.Optional[list]
+    scale: typing.Optional[list]
+    mesh: typing.Optional[typing.Dict]
+    children: typing.Dict
+
+
+
 
 class TransformationMxw(ExecutableHyperEdge):
 
     def __init__(self, name: str, timestamp: int, serial: int, guid: bytes, suid: bytes, label: str,
-                 parent: HypergraphElement = None, mxw_meta=None):
+                 parent: HypergraphElement = None, mxw_meta=None, units=None):
         super().__init__(name, timestamp, serial, guid, suid, label, parent)
         self._named_attr["mxw_meta"] = mxw_meta
+        self._named_attr["units"] = units
+
+    @staticmethod
+    def __orientation_order(euler, units):
+        if units.value["axis"].value.lower() == "xyz":
+            return euler[2], euler[0], euler[1]
+        elif units.value["axis"].value.lower() == "xzy":
+            return euler[1], euler[2], euler[0]
+        return euler[0], euler[1], euler[2]
+
+    def __convert_euler_to_quaternion(self, euler):
+
+        if self["units"] is not None:
+            if self["units"].value["angle"].value == "degree":
+                print(self.__orientation_order([math.radians(x) for x in euler], self["units"]))
+                x, y, z = self.__orientation_order([math.radians(x) for x in euler], self["units"])
+            elif self["units"].value["angle"].value == "radian":
+                x, y, z = self.__orientation_order(euler, self["units"])
+            else:
+                x, y, z = euler
+        else: # default
+            x, y, z = euler
+        x = x * 0.5
+        y = y * 0.5
+        z = z * 0.5
+        cr = math.cos(x)
+        sr = math.sin(x)
+        cp = math.cos(y)
+        sp = math.sin(y)
+        cy = math.cos(z)
+        sy = math.sin(z)
+        q = [0, 0, 0, 0]
+        q[0] = cy * cp * cr + sy * sp * sr
+        q[1] = sr * cp * cy - cr * sp * sy
+        q[2] = cr * sp * cy + sr * cp * sy
+        q[3] = cr * cp * sy - sr * sp * cy
+        return q
+
+    @staticmethod
+    def __position_axis_order(position, units):
+        if units.value["axis"].value.lower() == "xyz":
+            return position[0], position[2], position[1]
+        elif units.value["axis"].value.lower() == "xzy":
+            return position[0], position[1], position[2]
+        return position[1], position[0], position
+
+    def __create_mxw_node(self, r: HypergraphElement):
+        nmxw = MxwNode(r.name, None, None, None, None, {})
+        if "position" in r:
+            nmxw.position = self.__position_axis_order(r["position"].value, self["units"])
+        if "orientation" in r:
+            nmxw.orientation = self.__convert_euler_to_quaternion(r["orientation"].value)
+        if "scale" in r:
+            nmxw.scale = r["scale"].value
+        if "mesh" in r:
+            nmxw.mesh = {}
+            nmxw.mesh["url"] = r["mesh"]["url"].value
+            if "scale" in r["mesh"]:
+                nmxw.mesh["scale"] = r["mesh"]["scale"].value
+        return nmxw
+
+
+    def __mxw_node_process(self, node: MxwNode, n: HypergraphElement):
+        if n.count_composite_elements != 0:
+            res_node = self.op_mxw_node(n, depth=1)
+            for r in res_node:
+                nmxw = self.__create_mxw_node(r)
+                node.children[r.name] = nmxw
+                self.__mxw_node_process(nmxw, r)
+
+
 
 
     def operate(self, *args, **kwargs):
@@ -27,20 +112,11 @@ class TransformationMxw(ExecutableHyperEdge):
             stereotype=mxw_node
         )
         res_node = self.op_mxw_node(root, depth=1)
-        tree = {
-            'root': {
-                'child1': {
-                    'grandchild1': {},
-                    'grandchild2': {}
-                },
-                'child2': {
-                    'grandchild3': {},
-                    'grandchild4': {
-                        'greatgrandchild1': {}
-                    }
-                }
-            }
-        }
+        tree = None
+        for r in res_node:
+            if tree is None:
+                tree = self.__create_mxw_node(r)
+            self.__mxw_node_process(tree, r)
         print(os.getcwd())
         template_path = (os.path.join(os.path.dirname(__file__), 'templates'))
 
@@ -56,5 +132,7 @@ class TransformationMxw(ExecutableHyperEdge):
         # Rendereljük a sablont
         rendered_html = template.render(context)
         print(rendered_html)
+        with open(f"{root.name}.jsx", "w") as f:
+            f.write(rendered_html)
 
 
