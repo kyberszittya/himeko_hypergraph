08a61373b542c89704f943320975a48d79ce38f4
Hajdu Csaba <kyberszittya@protonmail.ch>
2024-08-31 13:13:44 +0200

    Added reference regeneration

--- src/himeko/transformations/text/generate_text.py
+++ src/himeko/transformations/text/generate_text.py
@@ -1,5 +1,19 @@
-from himeko.hbcm.elements.element import HypergraphElement
+from himeko.hbcm.elements.element import HypergraphElement, common_ancestor
 from himeko.hbcm.elements.interfaces.base_interfaces import IComposable
+
+
+def get_reference_name(referer: HypergraphElement, reference: HypergraphElement):
+    __ancestor = common_ancestor(referer, reference)
+
+    __el = reference
+    __res = []
+    while True:
+        if __el is None or __ancestor == __el:
+            break
+        __res.append(__el.name)
+        __el = __el.parent
+
+    return __res[::-1]
 
 
 def generate_signature_text(root: HypergraphElement):
@@ -7,7 +21,7 @@ def generate_signature_text(root: HypergraphElement):
     if len(root.stereotype) > 0:
         sig += ": "
         for st in root.stereotype:
-            sig += f" -> {st.name}"
+            sig += f" -> {'.'.join(get_reference_name(root, st))}"
     return sig + " {\n"
 
 
