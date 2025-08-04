f9df52e946bde74641f06cb2c71a5e6dcadccbce
kyberszittya <kyberszittya@protonmail.ch>
2025-08-05 00:24:01 +0200

    Added hypergraph

--- analysis/tensor_representation/hypergraphs.py
+++ analysis/tensor_representation/hypergraphs.py
@@ -191,3 +191,96 @@ class TinyHypergraph(HypergraphTensor):
     @property
     def e_dim(self):
         return TinyHypergraph.__e_dim
+    
+class SmallHypergraph2(HypergraphTensor):
+    __v_dim = 4
+    __e_dim = 2
+
+    def tensor_clique_expansion(self):
+        _e_dim = self.e_dim
+        _v_dim = self.v_dim
+        # Clique expansion tensor
+        tensor_e_1 = np.zeros((_e_dim, _v_dim, _v_dim))
+        tensor_e_1[0, :, :] = np.array([
+            [0, 1, 0, 1],
+            [1, 0, 0, 1],
+            [0, 0, 0, 0],
+            [1, 1, 0, 0]
+        ])
+        tensor_e_1[1, :, :] = np.array([
+            [0, 0, 0, 0],
+            [0, 0, 1, 0],
+            [0, 1, 0, 0],
+            [0, 0, 0, 0]        
+        ])
+        tensor_e_1[2, :, :] = np.array([
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 1],
+            [0, 0, 1, 0]
+        ])
+        tensor_e_1[3, :, :] = np.array([
+            [0, 0, 1, 1],
+            [0, 0, 0, 0],
+            [1, 0, 0, 1],
+            [1, 0, 1, 0]
+            
+        ])
+        return tensor_e_1
+
+
+    def tensor_star_expansion(self):
+        _e_dim = self.e_dim
+        _v_dim = self.v_dim
+        # Star expansion tensor
+        tensor_e_1 = np.zeros((_e_dim, _v_dim + _e_dim, _v_dim + _e_dim))
+        tensor_e_1[0, :, :] = np.array([
+            [0, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 0, 0],
+            [1, 1, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ])
+        tensor_e_1[1, :, :] = np.array([
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 1, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ])
+        tensor_e_1[2, :, :] = np.array([
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0]
+        ])
+        tensor_e_1[3, :, :] = np.array([
+            [0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 1, 1, 0, 0, 0, 0]
+        ])
+        return tensor_e_1
+    
+    
+    @property
+    def v_dim(self):
+        return SmallHypergraphTensor.__v_dim
+    
+    @property
+    def e_dim(self):
+        return SmallHypergraphTensor.__e_dim
